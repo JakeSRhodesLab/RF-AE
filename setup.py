bb6a47ea9afa614809bfa2d9f69037fe231b331d
aumona <adanamn1@gmail.com>
2025-11-03 16:59:59 -0500

    refactor setup.py to use parse_requirements function for dependencies

--- setup.py
+++ setup.py
@@ -1,20 +1,15 @@
 from setuptools import setup, find_packages
 
+def parse_requirements(filename):
+    with open(filename, 'r') as file:
+        lines = file.readlines()
+        return [line.strip() for line in lines]
+
 setup(
     name="rfae",  # Package name (used in imports)
     version="0.1.0",  # Version number
     packages=find_packages(),  # Automatically finds `rfae/` and submodules
-    install_requires=[
-        "numpy~=1.26.4",
-        "pandas~=2.0.3",
-        "scikit-learn~=1.4.0",
-        "torch~=2.2.0",
-        "scipy~=1.10.1",
-        "graphtools~=1.5.3",
-        "phate~=1.0.11",
-        "joblib~=1.3.2",
-        "rootutils~=1.0.7"
-    ],
+    install_requires=parse_requirements('requirements.txt'),
     author="JakeSRhodesLab",
     description = "Random Forest Autoencoders (RF-AE), a neural network-based framework for out-of-sample kernel extension that combines the flexibility of autoencoders with the supervised learning strengths of random forests and the geometry captured by RF-PHATE.", 
     url="https://github.com/JakeSRhodesLab/RF-AE",
